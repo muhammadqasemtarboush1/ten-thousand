ca64d780728108446406b7ca512319cc3624e88c
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-06-20 13:48:03 +0300

    style: refactor game play

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -14,6 +14,18 @@ class Game:
         self.banker = Banker()
         self.invalid_answer = False
 
+    def start_round(self):
+        print(f'You banked {self.banker.shelved} points in round {self.round}')
+        self.banker.bank()
+        print(f'Total score is {self.banker.balance} points')
+        self.round += 1
+        print(f'Starting round {self.round}')
+        self.dice = 6
+
+    def game_options(self):
+        print(f'You have {self.banker.shelved} unbanked points and {self.dice} dice remaining')
+        print('(r)oll again, (b)ank your points or (q)uit:')
+
     def zilch(self, new_roller):
         zilch_check_score = GameLogic.calculate_score(new_roller)
         if zilch_check_score == 0:
@@ -23,27 +35,24 @@ class Game:
             return True
         return False
 
+    def rolling_scouring(self, user_answer):
+        dices = [x for x in user_answer if x.strip() != ""]
+        num_of_dice_shelving = len(dices)
+        dice_kept = map(int, dices)
+        roll_tuple = tuple(dice_kept)
+        score = GameLogic.calculate_score(roll_tuple)
+        self.banker.shelf(score)
+        return num_of_dice_shelving
+
     def validate_shelfing(self, user_answer):
         if not self.invalid_answer:
-            dices = [x for x in user_answer if x.strip() != ""]
-            num_of_dice_shelving = len(dices)
-            dice_kept = map(int, dices)
-            roll_tuple = tuple(dice_kept)
-            score = GameLogic.calculate_score(roll_tuple)
-            self.banker.shelf(score)
+            num_of_dice_shelving = self.rolling_scouring(user_answer)
             self.dice -= num_of_dice_shelving
-            print(f'You have {self.banker.shelved} unbanked points and {self.dice} dice remaining')
-            print('(r)oll again, (b)ank your points or (q)uit:')
+            self.game_options()
         elif self.invalid_answer:
             print('Cheater!!! Or possibly made a typo...')
-            dices = [x for x in user_answer if x.strip() != ""]
-            num_of_dice_shelving = len(dices)
-            dice_kept = map(int, dices)
-            roll_tuple = tuple(dice_kept)
-            score = GameLogic.calculate_score(roll_tuple)
-            self.banker.shelf(score)
-            print(f'You have {self.banker.shelved} unbanked points and {self.dice} dice remaining')
-            print('(r)oll again, (b)ank your points or (q)uit:')
+            self.rolling_scouring(user_answer)
+            self.game_options()
 
     def play(self, roller=GameLogic.roll_dice):
         cheating_status = False
@@ -79,28 +88,17 @@ class Game:
                         if self.dice == 0 and user_choice == "r":
                             self.dice = 6
                         if user_choice == 'b':
-                            print(f'You banked {self.banker.shelved} points in round {self.round}')
-                            self.banker.bank()
-                            print(f'Total score is {self.banker.balance} points')
-                            self.round += 1
-                            print(f'Starting round {self.round}')
-                            self.dice = 6
+                            self.start_round()
                         if user_choice == 'q':
                             print(f'Thanks for playing. You earned {self.banker.balance} points')
                             return
             else:
-                print(f'You banked {self.banker.shelved} points in round {self.round}')
-                print(f'Total score is {self.banker.balance} points')
-                self.round += 1
-                print(f'Starting round {self.round}')
-                self.dice = 6
+                self.start_round()
 
 
 def check_cheater(user_answer, roller):
     dice_values = Counter(roller).most_common()
     user_input = Counter(user_answer).most_common()
-    # cheating_status = True
-    # print(dice_values)
     for i in user_input:
         for j in dice_values:
             if int(i[0]) == j[0] and i[1] <= j[1]:
